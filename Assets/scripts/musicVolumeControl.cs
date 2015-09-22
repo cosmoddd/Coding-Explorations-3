089697fa6813de6ce91bbfb184bf43a2ee0e9dea
Greg Heffernan <gregheff@gmail.com>
2015-09-22 14:49:06 -0400

    Figuring out the audio blending technqiues.

---
+++ Assets/scripts/musicVolumeControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicVolumeControl : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public LoopControl loopControl;
+    public float currentVolume;
+    public float target;
+    public int testLoopCount;
+    public float testLoopDuration;
+
+
+    // Use this for initialization
+    void Start () {
+
+        //thing.audioSource;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        testLoopDuration = loopControl.loopDuration;
+
+        updateVolume();
+
+        testLoopCount = loopControl.currentPlayCount;
+        if (testLoopCount == 1){
+            target = 1;
+        }
+        if (testLoopCount == loopControl.loopAmount)
+            {
+            target = 0;
+        }
+	}
+
+    void updateVolume()
+    {
+        audioSource.volume = currentVolume;
+        currentVolume = Mathf.SmoothStep(currentVolume, target, (Time.deltaTime*loopControl.loopDuration));
+    }
+
+}
